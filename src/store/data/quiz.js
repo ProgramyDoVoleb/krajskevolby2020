84ee36cb6b07fecc8a5f2e982cb35e908bcd82d5
Stepan Rysavy <git@programydovoleb.cz>
2020-07-19 13:55:10 +0200

    Quiz

---
+++ src/store/data/quiz.js
@@ -0,0 +1,61 @@
+{
+  demographics: {
+    region: null,
+    age: null,
+    sex: null
+  },
+  party: {
+    questions: [
+      {
+        question: 'Koho byste volili?',
+        answer: null,
+        optional: false
+      },
+      {
+        question: 'Kdo by byl vaše druhá volba?',
+        answer: null,
+        optional: true
+      },
+      {
+        question: 'Kdyby vaše strana sestavovala koalici, které strany by za vás byly akceptovatelné?',
+        answer: [],
+        optional: true
+      },
+      {
+        question: 'Kdyby vaše strana sestavovala koalici, které strany by naopak neměla do koalice přizvat?',
+        answer: [],
+        optional: true
+      },
+      {
+        question: 'Jsou nějaké strany, u kterých si vyloženě přejete, aby se do krajského zastupitelstva nedostaly?',
+        answer: [],
+        optional: true
+      }
+    ],
+    started: null,
+    completed: null,
+    token: null
+  },
+  hejtman: {
+    questions: [
+      {
+        question: 'Koho byste si přáli za hejtmana či hejtmanku?',
+        answer: [],
+        optional: false
+      },
+      {
+        question: 'Koho byste naopak nechtěli?',
+        answer: null,
+        optional: true
+      },
+      {
+        question: 'Kdo se podle vás nakonec hejtmanem stane?',
+        answer: null,
+        optional: true
+      }
+    ],
+    started: null,
+    completed: null,
+    token: null
+  }
+}
